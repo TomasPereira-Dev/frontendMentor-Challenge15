766cd49d2387e0651b0a3ab86b9ce8f5427dc7e4
Tomas Pereira <tomaspereira2k@gmail.com>
2024-01-02 17:21:44 -0300

    added tailwind custom colors, backgrounds and other minimal changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
     const [userScore, setUserScore] = useState(0)
     return(
         <>
+        <div>
             <div className='flex justify-between'>
                 <img src="./logo.svg" alt="the company's logo"/>
                 <div>
@@ -12,20 +13,22 @@ const App = () => {
                     <span>{userScore}</span>
                 </div>
             </div>
-            <div className='bg-triangle'>
-                <button type='button'>
+            <div className='bg-triangle bg-no-repeat bg-contain bg-center'>
+                <button className='flex items-center justify-center  max-h-[96px] max-w-[96px] p-6 bg-white rounded-full'>
                     <img src="./icon-paper.svg" alt="paper button"/>
                 </button>
-                <button type='button'>
+                <button className=' flex items-center justify-center max-h-[96px] max-w-[96px] p-6 bg-white rounded-full' type='button'>
                     <img src="./icon-scissors.svg" alt="sissors button"/>
                 </button>
-                <button type='button'>
+                <button className='flex items-center justify-center self-center p-6 bg-white rounded-full' type='button'>
                     <img src="./icon-rock.svg" alt="rock button"/>
                 </button>
             </div>
+
             <div>
                 <button type='button'>RULES</button>
             </div>
+        </div>
         </>
 
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,19 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        triangle: 'url(./bg-triangle.svg)'
+        triangle: 'url(./bg-triangle.svg)',
+        backgroundGradient: "radial-gradient(hsl(214, 47%, 23%), hsl(237, 49%, 15%))",
+        rockGradient: "radial-gradient(hsl(349, 71%, 52%), hsl(349, 70%, 56%))",
+        paperGradient: "radial-gradient(hsl(230, 89%, 62%), hsl(230, 89%, 65%))",
+        scissorsGradient: "radial-gradient(hsl(39, 89%, 49%) ,hsl(40, 84%, 53%))"
       },
       fontFamily: {
         sans: '"Barlow Semi Condensed", sans-serif'
+      },
+      colors: {
+        text1: "hsl(229, 25%, 31%)",
+        text2:"hsl(229, 64%, 46%)",
+        headerOutline: "hsl(217, 16%, 45%)",
       }
     },
   },
