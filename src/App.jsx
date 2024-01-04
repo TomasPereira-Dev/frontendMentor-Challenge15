c8fdeb06755a8eed4045744770832edca39c5fac
Tomas Pereira <tomaspereira2k@gmail.com>
2024-01-03 23:14:33 -0300

    minimal changes

--- src/App.jsx
+++ src/App.jsx
@@ -13,24 +13,20 @@ const App = () => {
                     <span>{userScore}</span>
                 </div>
             </div>
-            <div className='bg-triangle bg-no-repeat bg-contain bg-center'>
-                <button className='flex items-center justify-center  max-h-[96px] max-w-[96px] p-6 bg-white rounded-full'>
+            <div className='relative border-red-600 border-2 w-min'>
+                <img className='max-w-revert object-cover' src="./bg-triangle.svg" alt=""/>
+                <button className='absolute top-0 flex items-center justify-center  max-h-[96px] max-w-[96px] p-6 bg-white rounded-full'>
                     <img src="./icon-paper.svg" alt="paper button"/>
                 </button>
-                <button className=' flex items-center justify-center max-h-[96px] max-w-[96px] p-6 bg-white rounded-full' type='button'>
+                <button className='absolute top-0 right-0 flex items-center justify-center max-h-[96px] max-w-[96px] p-6 bg-white rounded-full' type='button'>
                     <img src="./icon-scissors.svg" alt="sissors button"/>
                 </button>
-                <button className='flex items-center justify-center self-center p-6 bg-white rounded-full' type='button'>
+                <button className='absolute bottom-0 z-40 mx-auto flex items-center justify-center p-6 bg-white rounded-full' type='button'>
                     <img src="./icon-rock.svg" alt="rock button"/>
                 </button>
             </div>
-
-            <div>
-                <button type='button'>RULES</button>
-            </div>
         </div>
         </>
-
     )
 }
 
